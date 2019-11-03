e3606b3ecc0d1f3fc7ea4ae97ffe3febf19e9f95
Kir Kup <k.bushmin@andersenlab.com>
2019-11-03 12:53:15 +0300

    second step

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,10 @@ import program from 'commander';
 
 program
   .version('1.0.0')
-  .description('Compares two configuration files and shows a difference.');
-
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => {
+    console.log(firstConfig, secondConfig);
+  });
 program.parse(process.argv);
